50760d3b27963321aaec96b9bb0ec4744410732e
Luís Felipe Rodrigues Valoura <luis@galltecnologia.com.br>
2023-01-12 20:31:22 -0300

    Capítulo 01 - Criação da aplicação no .NET 6 MVC

---
+++ Aplicacao/Controllers/InstituicaoController.cs
@@ -0,0 +1,104 @@
+﻿using Aplicacao.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacao.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> _instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                Id = 1,
+                Nome = "UniParaná",
+                Endereco = "Paraná"
+            },
+            new Instituicao()
+            {
+                Id = 2,
+                Nome = "UniSanta",
+                Endereco = "Santa Catarina"
+            },
+            new Instituicao()
+            {
+                Id = 3,
+                Nome = "UniSãoPaulo",
+                Endereco = "São Paulo"
+            },
+            new Instituicao()
+            {
+                Id = 4,
+                Nome = "UniGrandeSul",
+                Endereco = "Rio Grande do Sul"
+            },
+            new Instituicao()
+            {
+                Id = 5,
+                Nome = "UniCarioca",
+                Endereco = "Rio de Janeiro"
+            },
+        };
+
+
+        public IActionResult Index()
+        {
+            return View(_instituicoes);
+        }
+
+
+        [HttpGet]
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Criar(Instituicao novaInstituicao)
+        {
+            novaInstituicao.Id = _instituicoes.Max(I => I.Id) + 1;
+            _instituicoes.Add(novaInstituicao);
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            return View(_instituicoes.FirstOrDefault(I => I.Id == id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Instituicao instituicaoAlterada)
+        {
+            int posicao = _instituicoes.IndexOf(_instituicoes.First(I => I.Id == instituicaoAlterada.Id));
+            _instituicoes[posicao] = instituicaoAlterada;            
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Detalhes(int id)
+        {
+            return View(_instituicoes.First(I => I.Id == id));
+        }
+
+
+        [HttpGet]
+        public ActionResult Apagar(int id)
+        {
+            return View(_instituicoes.First(I => I.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Apagar(Instituicao instituicao)
+        {
+            _instituicoes.Remove(_instituicoes.First(I => I.Id == instituicao.Id));
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# .NET 6 MVC
+Livro ASPNET Core MVC - Projeto em .NET 6 MVC de gerenciamento de uma instituição de ensino superior
+
+### Artifícios em Destaque
+> * Desenvolvido em .NET 6 com arqueterura MVC
+> * Operações de CRUD de instituições
+
+### Alterações Feitas
+### Capítulo 01
+> * Criação do projeto
+> * Criando o controlador para Instituições
+> * Criação da classe de domínio para Instituições
+> * Implementação da interação da action Index com a View
+> * Implementação da inserção de dados no controlador
+> * Implementação da alteração de dados no controlador
+> * Implementação da visualização de um único registro
+> * Finalização da aplicação: a operação Delete
